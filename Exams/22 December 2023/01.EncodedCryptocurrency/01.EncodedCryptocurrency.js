ebd446d0adeda3bc29e3c911cbcb37c4e045cd70
paunovgeorgi <133797408+paunovgeorgi@users.noreply.github.com>
2024-04-12 17:19:21 +0300

    Update 01.EncodedCryptocurrency.js
    Redone from scratch,

--- Exams/22 December 2023/01.EncodedCryptocurrency/01.EncodedCryptocurrency.js
+++ Exams/22 December 2023/01.EncodedCryptocurrency/01.EncodedCryptocurrency.js
@@ -1,45 +1,45 @@
 function solve(input) {
+
     let message = input.shift();
 
     for (const line of input) {
-        if (line === 'TakeEven') {
-           message = takeEven(message);
-           console.log(message);
-        }
-        if (line.includes('ChangeAll')) {
-            let original = line.split('?')[1];
-            let replacement = line.split('?')[2];
-            while (message.includes(original)) {
-                message = message.replace(original, replacement);
+
+        let buy = false;
+        const [command, ...tokens] = line.split('?');
+        let subStr = tokens[0];
+        let replacement = tokens[1];
+
+        switch (command) {
+          case "TakeEven":
+            message = message.split('').filter((_, index) => index % 2 === 0).map(m => m).join('');
+            console.log(message);
+            break;
+          case "ChangeAll":
+            while (message.includes(subStr)) {
+                let temp = message.replace(subStr, replacement);
+                message = temp;
             }
             console.log(message);
-        }
-        if (line.includes('Reverse')) {
-            let subStr = line.split('?')[1];
+            break;
+          case "Reverse":
             if (message.includes(subStr)) {
-                let startIndex = message.indexOf(subStr);
-                let reversedSubStr = subStr.split('').reverse().join('');
-                let updateMsg = message.split('');
-                updateMsg.splice(startIndex, subStr.length);
-                message = updateMsg.join('') + reversedSubStr;
+                let reversed = subStr.split('').reverse().join('');
+                message = message.replace(subStr, '');
+                message += reversed;
                 console.log(message);
             }else{console.log('error');}
+            break;
+          case "Buy":
+            buy = true;
+            break;
         }
 
-        if (line === 'Buy') {
-            console.log(`The cryptocurrency is: ${message}`);
-            return;
+        if (buy) {
+            break;
         }
+        
     }
-
-    function takeEven(str) {
-        return str.split('').reduce((acc, curr, index) => {
-            if (index % 2 === 0) {
-                acc += curr;
-            }
-            return acc;
-        }, '')
-    }
+    console.log(`The cryptocurrency is: ${message}`);
 }
 
 
